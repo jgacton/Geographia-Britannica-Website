3dc2faaf166aa90ca7debb54999e644b325b5d9e
Josh Acton <jgacton@protonmail.com>
2024-12-26 18:42:04 +0000

    Added favicon image and headers

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import Header from './Header';
 import Navbar from './Navbar';
 import Footer from './Footer';
@@ -6,6 +7,12 @@ import Footer from './Footer';
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>Geographia Britannica</title>
+        <meta name="description" content="A complete, online, interactive geographical survey of Britain and its place in the world." /> 
+        <meta name="keywords" content="geography, Britain, online, interactive, map, atlas" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <Header />
       <Navbar />
       <main>{children}</main>
--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,16 @@ import Layout from '../components/Layout';
 
 const Home = () => {
   return (
-    <Layout>
-      <div className="main">
-        <br />
-        <p className="uppercase-subtitle">Atlas</p>
-        <p className="body-text">
-          A complete, online, interactive geographical survey of Britain and its place in the world.
-        </p>
-        <br />
-      </div>
-    </Layout>
+      <Layout>
+        <div className="main">
+          <br />
+          <p className="uppercase-subtitle">Atlas</p>
+          <p className="body-text">
+            A complete, online, interactive geographical survey of Britain and its place in the world.
+          </p>
+          <br />
+        </div>
+      </Layout>
   );
 };
 
