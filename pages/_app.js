be690e15ca4e73e16c58caa4fbf36dfb901775be
Josh Acton <jgacton@protonmail.com>
2024-12-31 16:18:34 +0100

    Fix analytics usage

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../public/styles/style.css';
 import '../public/styles/atlas.css';
 import Script from 'next/script';
+import { Analytics } from "@vercel/analytics/react"
 
 export default function MyApp({ Component, pageProps }) {
   return (
@@ -11,6 +12,7 @@ export default function MyApp({ Component, pageProps }) {
         strategy="lazyOnload" // This ensures the script loads after the page
       />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
